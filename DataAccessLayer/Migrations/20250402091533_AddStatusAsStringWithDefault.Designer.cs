b35e0f21b6655c8bf74d2b4c47ed27e65ea769e9
malmstrm <malmstrm.stefan@gmail.com>
2025-04-02 11:16:51 +0200

    Kunder står som active efter migration.

--- DataAccessLayer/Migrations/20250402091533_AddStatusAsStringWithDefault.Designer.cs
+++ DataAccessLayer/Migrations/20250402091533_AddStatusAsStringWithDefault.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(NordicBankAppDataContext))]
-    partial class NordicBankAppDataContextModelSnapshot : ModelSnapshot
+    [Migration("20250402091533_AddStatusAsStringWithDefault")]
+    partial class AddStatusAsStringWithDefault
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccessLayer/Migrations/20250402091533_AddStatusAsStringWithDefault.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusAsStringWithDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerStatus",
+                table: "Customers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "Active");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountStatus",
+                table: "Accounts",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "Active");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerStatus",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "AccountStatus",
+                table: "Accounts");
+        }
+    }
+}
