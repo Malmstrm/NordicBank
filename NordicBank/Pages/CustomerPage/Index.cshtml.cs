f6e780963eabfe4c5bdff523f25a6ea72907d2e5
malmstrm <malmstrm.stefan@gmail.com>
2025-04-02 12:36:12 +0200

    länk till customer skapad. DI Injected CustomerService i program.

--- NordicBank/Pages/CustomerPage/Index.cshtml.cs
+++ NordicBank/Pages/CustomerPage/Index.cshtml.cs
@@ -1,12 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NordicBank.ViewModels;
+using Services;
 
 namespace NordicBank.Pages.CustomerPage
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ICustomerService _customerService;
+        public IndexModel(ICustomerService customerService)
         {
+            _customerService = customerService;
+        }
+        public List<ViewCustomerViewModel> Customers { get; set; }
+        public async Task OnGetAsync()
+        {
+            var dtos = await _customerService.GetViewAsync();
+
+            Customers = dtos.Select(x => new ViewCustomerViewModel()
+            {
+                CustomerId = x.CustomerId,
+                Givenname = x.Givenname,
+                Streetaddress = x.Streetaddress,
+                City = x.City,
+                NationalId = x.NationalId,
+                Status = x.Status,
+            }).ToList();
         }
     }
 }
