490e7b43b46ea2e46efa6bf6be0674cda4ff2823
malmstrm <malmstrm.stefan@gmail.com>
2025-04-02 10:35:30 +0200

    DI injection, ändringar till min dataContext. delete old Data mapp

--- NordicBank/Program.cs
+++ NordicBank/Program.cs
@@ -1,17 +1,18 @@
+using DataAccessLayer.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using NordicBank.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<NordicBankAppDataContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<NordicBankAppDataContext>();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
