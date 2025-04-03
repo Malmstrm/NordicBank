4446229d831a430e46bd0eab809a160603668d9c
malmstrm <malmstrm.stefan@gmail.com>
2025-04-03 14:21:05 +0200

    DTO & Viewmodel har nu prop.

---
+++ DataAccessLayer/DTO/TransactionDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccessLayer.DTO
+{
+    public class TransactionDTO
+    {
+        public DateOnly Date { get; set; }
+        public string Type { get; set; } = "";
+        public string Operation { get; set; } = "";
+        public decimal Amount { get; set; }
+        public decimal Balance { get; set; }
+        public string? Description { get; set; }
+        public int AccountId { get; set; }
+    }
+}
---
+++ NordicBank/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace NordicBank.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public DateOnly Date {  get; set; }
+        public string Type { get; set; } = "";
+        public string Operation { get; set; } = "";
+        public decimal Amount { get; set; }
+        public decimal Balance { get; set; }
+        public string? Description { get; set; }
+        public int TransactionId { get; set; }
+        public int AccountId { get; set; }
+
+    }
+}
