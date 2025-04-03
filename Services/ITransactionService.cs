7fc726e81a54b66d541506ce229fa28ea2ef0b41
malmstrm <malmstrm.stefan@gmail.com>
2025-04-03 14:44:41 +0200

    Service, get transaction by accound id.

---
+++ Services/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface ITransactionService
+    {
+        Task<List<TransactionDTO>> GetTransactionsId(int id);
+    }
+}
---
+++ Services/TransactionService.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly NordicBankAppDataContext _dbContext;
+
+        public TransactionService(NordicBankAppDataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<TransactionDTO>> GetGetTransactionsId(int accountId)
+        {
+            return await _dbContext.Transactions
+                .Where(t => t.AccountId == accountId)
+                .Select(t => new TransactionDTO()
+                {
+                    Date = t.Date,
+                    Type = t.Type,
+                    Operation = t.Operation,
+                    Amount = t.Amount,
+                    Balance = t.Balance,
+                    Description = t.Symbol ?? t.Bank ?? t.Account ?? t.Operation,
+                    AccountId = accountId
+                })
+                .ToListAsync();
+        }
+    }
+}
