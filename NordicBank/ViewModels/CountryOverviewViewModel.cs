167a586d2c20bab74731cbed6170ef899e6ab611
malmstrm <malmstrm.stefan@gmail.com>
2025-04-06 15:35:28 +0200

    Viewmode props skapad

--- NordicBank/ViewModels/CountryOverviewViewModel.cs
+++ NordicBank/ViewModels/CountryOverviewViewModel.cs
@@ -2,5 +2,10 @@
 {
     public class CountryOverviewViewModel
     {
+        public string Country { get; set; }
+        public string CountryCode { get; set; }
+        public int Clients { get; set; }
+        public int Accounts { get; set; }
+        public decimal Capital  { get; set; }
     }
 }
