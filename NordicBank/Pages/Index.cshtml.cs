265ed09d25f0598a765e775ffc72591123cade8e
malmstrm <malmstrm.stefan@gmail.com>
2025-04-06 17:11:09 +0200

    Front & backend för Index sidan.

--- NordicBank/Pages/Index.cshtml.cs
+++ NordicBank/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NordicBank.Infrastructure.Paging.Country;
+using NordicBank.ViewModels;
 using Services;
 
 namespace NordicBank.Pages
@@ -13,11 +14,20 @@ namespace NordicBank.Pages
         {
             _countryOverviewService = countryOverviewService;
         }
-
+        public List<CountryOverviewViewModel> CountryOverview { get; set; }
 
         public async Task OnGetAsync()
         {
-
+            var dtoList = await _countryOverviewService.GetCountryOverviewAsync();
+            CountryOverview = dtoList.Select(d => new CountryOverviewViewModel()
+            {
+                Country = d.Country,
+                CountryCode = d.CountryCode,
+                Clients = d.Clients,
+                Accounts = d.Accounts,
+                Capital = d.Capital,
+            })
+                .ToList();
         }
     }
 }
