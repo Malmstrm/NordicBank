8f5fe4a9fadc24cca4e0a56af21ff36ebeeea7f6
malmstrm <malmstrm.stefan@gmail.com>
2025-04-06 19:11:46 +0200

    Front&Backend kod, partial update. Modal I Details samt JS för stödja det

--- NordicBank/Pages/CustomerPage/Details.cshtml.cs
+++ NordicBank/Pages/CustomerPage/Details.cshtml.cs
@@ -1,6 +1,7 @@
 using DataAccessLayer.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using NordicBank.ViewModels;
 using Services;
 
@@ -106,6 +107,37 @@ namespace NordicBank.Pages.CustomerPage
             if(!success) return NotFound();
 
             return RedirectToPage("Index");
+        }
+        public async Task<PartialViewResult> OnGetMoreInfoPartialAsync(int customerId)
+        {
+            var customer = await _customerService.GetByIdAsyn(customerId); // använd din befintliga DTO
+
+            var vm = new CustomerDetailsViewModel
+            {
+                CustomerId = customer.CustomerId,
+                Givenname = customer.Givenname,
+                Surname = customer.Surname,
+                Gender = customer.Gender,
+                Streetaddress = customer.Streetaddress,
+                City = customer.City,
+                Zipcode = customer.Zipcode,
+                Country = customer.Country,
+                CountryCode = customer.CountryCode,
+                Birthday = customer.Birthday,
+                NationalId = customer.NationalId,
+                Telephonecountrycode = customer.Telephonecountrycode,
+                Telephonenumber = customer.Telephonenumber,
+                Emailaddress = customer.Emailaddress,
+                Status = customer.Status,
+                NumberOfAccounts = await _customerService.GetAccountCountAsync(customerId),
+                TotalBalance = await _customerService.GetTotalBalanceAsync(customerId)
+            };
+
+            return new PartialViewResult
+            {
+                ViewName = "Partials/_CustomerDetailsPartial",
+                ViewData = new ViewDataDictionary<CustomerDetailsViewModel>(ViewData, vm)
+            };
         }
     }
 }
