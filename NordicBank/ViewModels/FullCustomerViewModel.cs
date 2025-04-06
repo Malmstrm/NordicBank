798e17c39d7a737e348b9bf286b30b13199e5746
malmstrm <malmstrm.stefan@gmail.com>
2025-04-06 18:50:10 +0200

    ViewModel Fullcustomer

---
+++ NordicBank/ViewModels/FullCustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace NordicBank.ViewModels
+{
+    public class FullCustomerViewModel
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string NationalId { get; set; }
+        public int AccountCount { get; set; }
+    }
+}
