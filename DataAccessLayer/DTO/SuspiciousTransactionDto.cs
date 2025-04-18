5f696abc13581293342eee7735885231f68468fe
malmstrm <malmstrm.stefan@gmail.com>
2025-04-18 17:19:20 +0200

    DTO, interface för AML

---
+++ DataAccessLayer/DTO/SuspiciousTransactionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccessLayer.DTO
+{
+    public class SuspiciousTransactionDTO
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; } = "";
+        public int AccountId { get; set; }
+        public int TransactionId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Reason { get; set; } = "";
+    }
+}
---
+++ Services/IAntiMoneyLaunderingService.cs
@@ -0,0 +1,10 @@
+﻿using DataAccessLayer.DTO;
+
+namespace Services
+{
+    public interface IAntiMoneyLaunderingService
+    {
+        Task<ScanLogDTO> RunScanAsync(string country, DateTime endDate);
+        Task<DateTime> GetEarliestTransactionDateAsync(string country);
+    }
+}
