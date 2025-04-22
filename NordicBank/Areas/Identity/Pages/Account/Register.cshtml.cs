0db776a27a595d5e034ac24fd5f8fcc7f47bfb22
malmstrm <malmstrm.stefan@gmail.com>
2025-04-22 09:44:57 +0200

    Ändringari layout, rensat i register. samt lagt till efter reggad user så tillbaka till admin.

--- NordicBank/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ NordicBank/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,22 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
 
 namespace NordicBank.Areas.Identity.Pages.Account
 {
@@ -164,7 +157,7 @@ namespace NordicBank.Areas.Identity.Pages.Account
             }
 
             // If we got this far, something failed, redisplay form
-            return Page();
+            return RedirectToPage("/Admin/Users/Index", new { area = "" });
         }
 
         private IdentityUser CreateUser()
